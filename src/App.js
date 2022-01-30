97234277e8c4bfaea728be8c4d94a68d23e10a38
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-01-30 08:42:34 -0300

    finish todo app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  const [todos, setTodos] = React.useState([]);
+  const [newTodo, setNewTodo] = React.useState("");
+
+  React.useEffect(() => {
+    const storedTodos = window.localStorage.getItem("todos");
+
+    if (storedTodos) {
+      setTodos(JSON.parse(storedTodos));
+    }
+  }, []);
+
+  React.useEffect(() => {
+    window.localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const addNewTodo = (e) => {
+    e.preventDefault();
+
+    const updatedTodos = [...todos, newTodo];
+    setTodos(updatedTodos);
+
+    setNewTodo("");
+  };
+
+  const removeTodo = (e) => {
+    const todoToRemoveContent =
+      e.target.parentElement.querySelector("span").textContent;
+
+    const updatedTodos = todos.filter((todo) => todo !== todoToRemoveContent);
+
+    setTodos(updatedTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main>
+        <h1>Todo list</h1>
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo}>
+              <span>{todo}</span>{" "}
+              <button onClick={removeTodo}>remover item</button>
+            </li>
+          ))}
+        </ul>
+        <form>
+          <input
+            type="text"
+            id="add"
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+          ></input>
+          <button type="submit" onClick={addNewTodo}>
+            Adicionar novo item
+          </button>
+        </form>
+      </main>
+    </>
   );
 }
 
